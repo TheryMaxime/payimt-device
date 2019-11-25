30e0c3d27d68e2e57d4dec5815cdfa1c169ad992
TheryMaxime <thery_maxime@yahoo.fr>
2019-11-25 10:00:14 +0100

    adding settings view

---
+++ Components/Settings.js
@@ -0,0 +1,16 @@
+//Components/TopBar.js
+
+import React from 'react'
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
+
+class Settings extends React.Component {
+
+  render() {
+    return(
+      <Text>Settings</Text>
+    )
+  }
+
+}
+
+export default Settings
