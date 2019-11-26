0beed04f5fc60000ededd93d9726ae00059d7bba
Antoine <bouchez.antoine@outlook.fr>
2019-11-26 17:37:08 +0100

    OK

--- API/LydiaAPI.js
+++ API/LydiaAPI.js
@@ -1,18 +1,7 @@
 // API/LydiaAPI.js
 import md5 from 'js-md5'
 
-<<<<<<< HEAD
-const API_PUBLIC_TOKEN_PROD = "5ac63d8703fc4447001415" //--> Vendor token (prod)
-const API_PRIVATE_TOKEN_PROD = "5ac63d8715eb7991881236" //--> private prod
-
-const API_PUBLIC_TOKEN_TESTS = "5d95c265636cb822765161" //--> Vendor token test (en attente)
-const API_PRIVATE_TOKEN_TESTS = "5d95c26567952842723380" //--> private tests (en attente)
-
-const NUM_LYDIA_TEST = "+33621491838"
-const NUM_LYDIA_TEST_MD5 = md5(NUM_LYDIA_TEST)
-=======
 import LydiaConfiguration from './configuration/LydiaConfiguration'
->>>>>>> develop
 
 const API_PUBLIC_TOKEN_PROD = LydiaConfiguration.API_PUBLIC_TOKEN_PROD //--> Vendor token (prod)
 const API_PRIVATE_TOKEN_PRO = LydiaConfiguration.API_PRIVATE_TOKEN_PRO //--> private prod
